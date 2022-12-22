5fe01f23e9fdbe16bad162a2af9c657a1ad0abb0
Atif Burki <atifnoori.official@gmail.com>
2022-12-22 12:17:48 +0500

    YT Downloader

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -4,8 +4,8 @@ import 'package:share_plus/share_plus.dart';
 class Constants{
 
   //static const String APP_ID = 'ca-app-pub-5818564663446600~3801225236';
-  static const String BANNER_ID = 'ca-app-pub-5818564663446600/7902716800';
-  static const String INTERSTITIAL_ID = 'ca-app-pub-5818564663446600/6589635134';
+  static const String BANNER_ID = 'ca-app-pub-3940256099942544/6300978111';
+  static const String INTERSTITIAL_ID = 'ca-app-pub-3940256099942544/1033173712';
 
   static void showToast({String? msg}){
     Fluttertoast.showToast(
